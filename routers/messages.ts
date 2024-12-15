8bafb6d5db79e66e7f1af0a200b8a683c7f63515
Akylbek <hsplay74@gmail.com>
2024-12-15 23:02:30 +0600

    Routes

--- routers/messages.ts
+++ routers/messages.ts
@@ -1,13 +1,24 @@
 import express from "express";
+import fileDb from "../fileDb";
+import {MessageWithoutId} from "../types";
 
 const messagesRouter = express.Router();
 
-messagesRouter.get("/", (req, res) => {
-    res.status(200).send("Messages page loaded");
+messagesRouter.get("/", async (req, res) => {
+    await fileDb.init();
+    const msg = await fileDb.getMessage();
+    res.send(msg.reverse().slice(0, 5))
 });
 
-messagesRouter.post("/", (req, res) => {
-    res.status(200).send("Messages page loaded");
+messagesRouter.post("/", async (req, res) => {
+    const date = new Date();
+    const msg: MessageWithoutId = {
+        message: req.body.message,
+        datetime: `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}-${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}:${String(date.getSeconds()).padStart(2, '0')}`
+    }
+
+    await fileDb.addMessage(msg)
+    res.send(msg)
 });
 
 
